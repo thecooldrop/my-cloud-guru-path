ba3bd7163c3be76f83aecf03913bf87d63233463
Vanio Begic <vanio.begic@capgemini.com>
2024-03-01 23:43:38 +0100

    Add next labs

---
+++ aws-certified-cloud-practitioner/creating-s3-buckets-with-versioning-and-encryption/main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.38.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-central-1"
+}
+
+resource "aws_s3_bucket" "bucket" {
+}
+
+resource "aws_s3_bucket_public_access_block" "bucket-block-public-access" {
+  bucket = aws_s3_bucket.bucket.id
+  block_public_acls = true
+  block_public_policy = true
+  ignore_public_acls = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "bucket-server-side-encryption" {
+  bucket = aws_s3_bucket.bucket.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "aws:kms"
+    }
+  }
+}
+
+resource "aws_s3_bucket_versioning" "bucket-versioning" {
+  bucket = aws_s3_bucket.bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
